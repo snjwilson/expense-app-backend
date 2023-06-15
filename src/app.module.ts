3a8fc4868e00b73466058eb081d91022d88a18c0
snjwilson <sanjaywilsonktm@gmail.com>
2023-06-16 03:33:47 +0530

    Add auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ExpensesModule } from './expenses/expenses.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/expenses'),
+    AuthModule,
     UsersModule,
     ExpensesModule,
   ],
